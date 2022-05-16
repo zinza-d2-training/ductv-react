fe2b8e0441bc3ee42769ebff7573f84ba2e228ca
duczinza <duc.tv@zinza.com.vn>
2022-05-16 11:36:55 +0700

    add packet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-    
-    </Typography>
-  );
-}
+import { Button } from '@mui/material';
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/vietnamese-300.css';
+import '@fontsource/roboto/vietnamese-400.css';
+import '@fontsource/roboto/vietnamese-500.css';
+import '@fontsource/roboto/vietnamese-700.css';
+import { ThemeProvider } from '@mui/material';
 import theme from './theme';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement!);
-
-root.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
